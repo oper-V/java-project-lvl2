4561f4a230084c720880a5277520e7407a7f6ccb
Igor Prokhorenko <igor.prohorenko@gmail.com>
2021-06-05 11:46:30 +0300

    Выполнен 4 шаг проекта "Вычислитель отличий". Настроен прием из командной строки аргументов и параметров для выполнения приложения. The 4th step of the "Difference calculator" project has been completed. Configured to accept command line arguments and parameters for application execution.

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
+import java.io.File;
 
 /*Для работы приложения в командной строке необходимо реализовать интерфейс Callable,
  *поэтому синтаксический анализ, обработка ошибок и обработка пользовательских запросов на
@@ -15,6 +16,17 @@ import java.util.concurrent.Callable;
          description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "path to second  file")
+    private File filepath2;      
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish"; 
+
+
     @Override
     public Integer call() throws Exception { // your business logic goes here...
  	System.out.println("Hello World!");
